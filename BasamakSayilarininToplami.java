4191485102d84b68ce025585a772f7164a660490
Sedat Ay <159232350+SedatAy1@users.noreply.github.com>
2024-02-27 16:47:24 +0300

    BasamakSayilarininToplami
    BasamakSayilarininToplami

---
+++ BasamakSayilarininToplami.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class BasamakSayilarininToplami {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+
+        System.out.print("Bir sayı girin: ");
+        int sayi = scanner.nextInt();
+
+
+        int toplam = 0;
+        int kalan;
+
+
+        while (sayi != 0) {
+            kalan = sayi % 10;
+            toplam += kalan; /
+            sayi /= 10; //
+        }
+
+        System.out.println("Basamakların toplamı: " + toplam);
+
+
+    }
+}
